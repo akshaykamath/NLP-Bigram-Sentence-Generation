9987b7c17fea2f913de3c2a92e4c0f6629e0a42a
Akshay Kamath <AKamath@cigital.com>
2016-09-10 20:45:03 -0400

    Initial checkin

---
+++ BigramProbabilities.py
@@ -0,0 +1,181 @@
+from __future__ import division
+from collections import defaultdict
+
+wordCount = 0
+
+
+def add_single_item_to_dict(dict, item):
+    if item not in dict:
+        dict[item] = 1
+    else:
+        dict[item] += 1
+
+
+def readFile():
+    with open('Dataset\\nonsense - Copy.txt') as f:
+        lines = f.read().splitlines()
+    return lines
+
+
+def print_size_of_vocab():
+    size = 0
+    for word in vocabulary.keys():
+        size += vocabulary[word]
+        #print word
+
+    print "vocab word count", size
+    print "vocab size count", len(vocabulary.keys())
+
+
+def print_size_of_extended_vocab():
+    size = 0
+    for word in extended_vocabulary.keys():
+        size += extended_vocabulary[word]
+        #print word
+
+    print "extended vocab word count", size
+    print "extended vocab size count", len(extended_vocabulary.keys())
+
+
+def calculate_vocab():
+    bigram_count = 0
+    wordCount = 0
+    lines = readFile()
+
+    for line in lines:
+        sentence = line.split()
+
+        for i in range(0, len(sentence)):
+            word = sentence[i]
+
+            add_single_item_to_dict(vocabulary, word)
+
+            wordCount += 1
+
+            if word == "START123" or word == "END123":
+                continue
+
+            else:
+                previousWord = sentence[i - 1]
+                bigram = (previousWord, word)
+                add_single_item_to_dict(bigram_dict, bigram)
+                bigram_count += 1
+
+
+def calculate_extended_vocab():
+    wordCount = 0
+    for line in extended_vocabulary_arr:
+        sentence = line.split()
+
+        for i in range(0, len(sentence)):
+            word = sentence[i]
+
+            if word not in vocabulary:
+                add_single_item_to_dict(extended_vocabulary, word)
+
+            wordCount += 1
+
+    print wordCount
+
+
+def bigram_probability():
+    for bigram in bigram_dict.keys():
+        try:
+            probability = float(bigram_dict[bigram] / vocabulary[bigram[0]])
+            bigram_probability_dict[bigram] = probability
+            print bigram, ": ", probability
+        except:
+            bigram_probability_dict[bigram] = "NAN"
+            print bigram, ": NAN"
+
+    return bigram_probability_dict
+
+
+def bigram_additive_smoothing_probability():
+    print len(vocabulary.keys())
+    size_of_vocabulary = len(vocabulary.keys()) - 3
+
+    for bigram in bigram_dict.keys():
+        try:
+            probability = float( (bigram_dict[bigram] + 1)/ (vocabulary[bigram[0]] + size_of_vocabulary))
+            bigram_probability_dict[bigram] = probability
+            print bigram, ": ", probability
+        except:
+            bigram_probability_dict[bigram] = "NAN"
+            print bigram, ": NAN"
+
+    return bigram_probability_dict
+
+
+# Assumption here that the dcitionary being passed is the laplace smoothed vocab.
+def get_sentence_probability(sentence, bigram_probability_dict):
+    words = sentence.split()
+    sentence_probability = 1
+
+    # Subtract 4 for start and end markers in both the vocabs.
+    vocab_size = len(vocabulary.keys()) + len(extended_vocabulary.keys()) - 2
+
+    for i in range(0, len(words)):
+        word = words[i]
+        if word == "START123" or word == "END123":
+            continue
+
+        previousWord = words[i - 1]
+
+        if (previousWord, word) in bigram_probability_dict:
+            sentence_probability = sentence_probability * bigram_probability_dict[(previousWord, word)]
+        else:
+            denominator = 0
+            # if previous word is in vocabulary, get the occurences of that word.
+            # if not, just add the size of the vocab.
+
+            if previousWord in vocabulary:
+                denominator = vocabulary[previousWord]
+
+            denominator += vocab_size
+            bigram_probability = float(1 / denominator)
+            sentence_probability = sentence_probability * bigram_probability
+
+    print sentence, " ", sentence_probability
+    return sentence_probability
+
+extended_vocabulary_arr = ["START123 I do not like them in a mouse . END123",
+                           "START123 I am Sam I am Sam END123",
+                           "START123 I do like them anywhere . END123",
+                           "START123 I would like green ham and beef . END123"]
+
+
+vocabulary = defaultdict(int)
+bigram_dict = defaultdict(int)
+bigram_probability_dict = defaultdict(int)
+extended_vocabulary = defaultdict(int)
+
+print "\n##################################################################\n"
+calculate_vocab()
+calculate_extended_vocab()
+print_size_of_vocab()
+print_size_of_extended_vocab()
+
+
+print "\n##################################################################\n"
+bigram_probability()
+
+print "\n##################################################################\n"
+bigram_prob_dict = bigram_additive_smoothing_probability()
+
+print "\n##################################################################\n"
+get_sentence_probability("START123  I do not like them  with a mouse . END123", bigram_prob_dict)
+
+print "\n##################################################################\n"
+get_sentence_probability("START123 I am Sam I am Sam END123", bigram_prob_dict)
+
+print "\n##################################################################\n"
+get_sentence_probability("START123 I do like them anywhere . END123", bigram_prob_dict)
+
+print "\n##################################################################\n"
+get_sentence_probability("START123  I do not like them  with a mouse . END123", bigram_prob_dict)
+
+print "\n##################################################################\n"
+get_sentence_probability("START123 I would like green ham and beef . END123", bigram_prob_dict)
+
+
